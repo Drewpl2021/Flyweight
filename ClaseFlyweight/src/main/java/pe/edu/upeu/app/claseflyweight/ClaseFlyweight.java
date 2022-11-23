80ae79c318b09f800311283082f5314ca9ec295e
Drewpl2021 <andres.montes@cata.edu.pe>
2022-11-22 19:18:11 -0500

    Actual

--- ClaseFlyweight/src/main/java/pe/edu/upeu/app/claseflyweight/ClaseFlyweight.java
+++ ClaseFlyweight/src/main/java/pe/edu/upeu/app/claseflyweight/ClaseFlyweight.java
@@ -12,6 +12,6 @@ package pe.edu.upeu.app.claseflyweight;
 public class ClaseFlyweight {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola panas");
     }
 }
